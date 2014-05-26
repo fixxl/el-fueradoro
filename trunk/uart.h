d61c1ab75a87847c2d7e1e34c5f8ec35dc2e48e0
f.pflaum@gmail.com <f.pflaum@gmail.com@0514aee9-97dd-25bc-fcb9-e4662e86089c>
2014-05-26 08:21:38 +0000

    Defined some function-input-pointers as "const" to make sure they don't get changed by the function.

--- trunk/uart.h
+++ trunk/uart.h
@@ -11,7 +11,7 @@
 #define RXD 		PD0 // Eingang: Empfänger
 #define TXD 		PD1	// Ausgang: Sender
 #define RTS 		PD2 // Ausgang: Darf die Gegenstelle Daten schicken? (HIGH=nein, LOW=ja)
-#define CTS 		PD3 // Eingang: Darf man Daten an Gegenstelle senden? (HIGH=nein, LOW=ja?)
+#define CTS 		PD3 // Eingang: Darf man Daten an Gegenstelle senden? (HIGH=nein, LOW=ja)
 #define UART_PORT 	PORTD
 #define UART_DDR	DDRD
 #define UART_PIN	PIND
@@ -27,8 +27,8 @@ void uart_gets(char *s);
 uint8_t uart_putc(uint8_t c);
 void uart_puts (char *s);
 void uart_puts_P (const char *s);
-uint8_t uart_strings_equal(char* string1, char* string2);
-uint8_t uart_valid(char *field);
+uint8_t uart_strings_equal(const char* string1, const char* string2);
+uint8_t uart_valid(const char *field);
 void uart_shownum(int32_t zahl, uint8_t type);
 
 
