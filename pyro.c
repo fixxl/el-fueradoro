709509cbe799b8297250b35303493762bd6d7f20
f.pflaum@gmail.com <f.pflaum@gmail.com@0514aee9-97dd-25bc-fcb9-e4662e86089c>
2014-05-27 07:10:25 +0000

    Completely translated comments and annotations to English.

--- pyro.c
+++ pyro.c
@@ -74,11 +74,12 @@ uint8_t debounce(volatile uint8_t *port, uint8_t pin) {
 	while (((keystate != 0x00) && (keystate != 0xFF)) || (ctr < 8)) {
 		keystate <<= 1;								// Shift left
 		keystate |= ((*port & (1 << pin)) > 0);		// Write 0 or 1 to LSB
-		while (!(TIFR0 & (1 << TOV0)))				// Wait for timer overflow
+		while (!(TIFR0 & (1 << TOV0)))
+			// Wait for timer overflow
 			;
 		TIFR0 = (1 << TOV0);						// Clear interrupt flag
 		TCNT0 = 160;								// Preload timer
-		if(ctr<8) ctr++;							// Make sure at least 8 queries are executed
+		if (ctr < 8) ctr++;							// Make sure at least 8 queries are executed
 	}
 	TCCR0B = timer0_regb;
 
@@ -184,7 +185,6 @@ uint8_t uart_valid(const char *field) {
 }
 
 // ------------------------------------------------------------------------------------------------------------------------
-
 
 // Shift LCD-cursor (line 3 and 4)
 void cursor_x_shift(uint8_t* last_zeile, uint8_t* last_spalte, uint8_t* anz_zeile, uint8_t* anz_spalte) {
@@ -374,7 +374,7 @@ int main(void) {
 
 			// Any other command is received and the first char prefixed afterwards
 			else {
-				uart_putc(uart_char);						// Show first char so everything looks as it should
+				uart_putc(uart_char);					// Show first char so everything looks as it should
 				tmp = uart_gets(uart_field);				// Get number of received chars
 				uart_field[tmp + 1] = '\0';					// Extend length by 1 char
 				for (uint8_t i = tmp; i; i--) {
@@ -466,7 +466,7 @@ int main(void) {
 
 // -------------------------------------------------------------------------------------------------------
 
-// Temperaturmessung
+// Temperature Measurement
 		if (flags.b.temp) {
 			temp_sreg = SREG;
 			cli();
@@ -483,13 +483,12 @@ int main(void) {
 			}
 			uart_puts_P(PSTR("\n\r\n\r"));
 
-			if(SENDERBOX) {
+			if (SENDERBOX) {
 				lcd_cursorset(3, 1);
 				lcd_puts("Temperatur:    ");
 				if (temperature == -128) {
 					lcd_puts("n.a. ");
-				}
-				else {
+				} else {
 					lcd_arrize(temperature, lcd_array, 2, 1);
 					lcd_puts(lcd_array);
 					lcd_send(1, 1);
@@ -502,7 +501,7 @@ int main(void) {
 
 // -------------------------------------------------------------------------------------------------------
 
-// Einstellungen zu Slave- und Unique-ID
+// Slave- and Unique-ID settings
 		if (flags.b.uart_config) {
 			temp_sreg = SREG;
 			cli();
@@ -518,7 +517,7 @@ int main(void) {
 
 // -------------------------------------------------------------------------------------------------------
 
-// Feuern
+// Fire
 		if (flags.b.fire) {
 			temp_sreg = SREG;
 			cli();
@@ -531,15 +530,15 @@ int main(void) {
 			if (rx_field[2] > 0 && rx_field[2] < 17 && armed) {
 				tmp = rx_field[2];
 				scheme = 0;
-				for (uint8_t i = 16; i > 0; i--) {
+				for (uint8_t i = 16; i; i--) {
 					scheme <<= 1;
 					if (i == tmp) scheme |= 1;
 				}
 				sr_shiftout(scheme);
 
-				/* Um im Falle einen Kurzschlusses nach der Zündung den MOSFET nicht zu beschädigen/zerstören
-				 * wird der Kanal nach 11ms automatisch wieder gesperrt. Gemäß Spezifikation müssen die Zünder
-				 * bis dahin ausgelöst haben
+				/*
+				 * To avoid demage to the MOSFET in case of a short circuit after ignition, the MOSFET is set to
+				 * blocking state again after 11ms. According to specification the detonator must have exploded by then.
 				 */
 				_delay_ms(11);
 				sr_shiftout(0);
@@ -551,7 +550,7 @@ int main(void) {
 
 // -------------------------------------------------------------------------------------------------------
 
-// Geschossene Kanäle zurücksetzen
+// Reset fired channels
 		if (flags.b.reset_fired) {
 			temp_sreg = SREG;
 			cli();
@@ -566,7 +565,7 @@ int main(void) {
 
 // -------------------------------------------------------------------------------------------------------
 
-// Manuelles Senden
+// Manual transmission
 		if (flags.b.send) {
 			temp_sreg = SREG;
 			cli();
@@ -640,7 +639,7 @@ int main(void) {
 
 // -------------------------------------------------------------------------------------------------------
 
-// Auflisten
+// List network devices
 		if (flags.b.list) {
 			temp_sreg = SREG;
 			cli();
@@ -656,7 +655,7 @@ int main(void) {
 
 // -------------------------------------------------------------------------------------------------------
 
-// Reset auslösen
+// Software-Reset via Watchdog
 		if (flags.b.reset_device) {
 			cli();
 			if (SENDERBOX) {
@@ -671,7 +670,7 @@ int main(void) {
 
 // -------------------------------------------------------------------------------------------------------
 
-// Vor Identifikationsaufforderung Liste löschen
+// Clear list of ignition devices
 		if (flags.b.clear_list || (flags.b.transmit && tx_field[0] == IDENT)) {
 			temp_sreg = SREG;
 			cli();
@@ -693,48 +692,51 @@ int main(void) {
 
 // -------------------------------------------------------------------------------------------------------
 
-// Senderoutine
-		if (flags.b.transmit || transmit_flag > 3) { // Wenn Befehl gesendet werden soll...
-			temp_sreg = SREG; // Speichere Statusregister
+// Transmit
+		if (flags.b.transmit || transmit_flag > 3) {
+			temp_sreg = SREG;
 			cli();
 
-			flags.b.transmit = 0; // Lösche Sendeaufforderung
+			flags.b.transmit = 0;
 			transmit_flag = 0;
 
 			led_green_on();
-			rfm_transmit(tx_field, tx_length); // Übertrage Nachricht
-			if (tx_field[0] == FIRE) while (--tx_field[3])
-				rfm_transmit(tx_field, tx_length);
+			rfm_transmit(tx_field, tx_length); 	// Transmit message
+			if (tx_field[0] == FIRE) {			// Repeat ignition command a certain amount of times (FIREREPEATS)
+				while (--tx_field[3]) {
+					rfm_transmit(tx_field, tx_length);
+				}
+			}
 			led_green_off();
 
 			flags.b.lcd_update = 1;
 			flags.b.tx_post = 1;
 
-			SREG = temp_sreg; // Stelle Statusregisterzustand wieder her
+			SREG = temp_sreg;
 		}
 
 // -------------------------------------------------------------------------------------------------------
 
-// Empfangsroutine
+// Receive
 		flags.b.receive = (rfm_receiving() ? 1 : 0);
-		if (flags.b.receive) { // Wenn Byte/Paket angekommen
-			temp_sreg = SREG; // Speichere Statusregister
+		if (flags.b.receive) {
+			temp_sreg = SREG;
 			cli();
 
 			flags.b.receive = 0;
 
 			led_blue_on();
 #ifdef RFM69_H_
-			rssi = rfm_get_rssi_dbm(); // Senderbox misst Signalstärke
+			rssi = rfm_get_rssi_dbm(); // Measure signal strength (RFM69 only)
 #endif
-			success = rfm_receive(rx_field, &rx_length); // Hole Nachricht ab
+			success = rfm_receive(rx_field, &rx_length); // Get Message
 			led_blue_off();
 
-			if (!success) rx_field[0] = ERROR; // Werte aus
+			if (!success) rx_field[0] = ERROR;
 			else {
-				switch (rx_field[0]) { // Weiteres Vorgehen anhand der Art der Nachricht
+				switch (rx_field[0]) { 	// Act according to type of message received
 
-				// Zündbefehl erhalten (nur für Zündboxen relevant)
+				// Received ignition command (only relevant for ignition devices)
 				case FIRE: {
 					if ((rx_field[1] == slave_id) && !SENDERBOX) {
 						tmp = rx_field[2] - 1;
@@ -747,7 +749,7 @@ int main(void) {
 					break;
 				}
 
-					// Aufforderung zur Identifizierung erhalten (nur für Zündboxen relevant)
+					// Received identification-demand
 				case IDENT: {
 					rfm_rxoff();
 					tx_field[0] = PARAMETERS;
@@ -763,7 +765,7 @@ int main(void) {
 					break;
 				}
 
-					// Parameter erhalten (nur für Senderbox relevant)
+					// Received Parameters (only relevant for transmitter)
 				case PARAMETERS: {
 					if (SENDERBOX) {
 						if (rx_field[2] == 'E' || !rx_field[2]) {
@@ -780,15 +782,16 @@ int main(void) {
 					break;
 				}
 
-					// Standardaktion
+					// Default action (do nothing)
 				default: {
 					break;
 				}
 				}
 
+				// Make sure all devices answer one after the other
 				if (flags.b.transmit) {
 					for (i = 0; i < unique_id; i++) {
-						_delay_ms(25);
+						_delay_ms(33);
 					}
 				} else {
 					rfm_rxon();
@@ -796,7 +799,7 @@ int main(void) {
 				flags.b.lcd_update = 1;
 				flags.b.rx_post = 1;
 			}
-			SREG = temp_sreg; // Stelle Statusregisterzustand wieder her
+			SREG = temp_sreg;
 		}
 
 // -------------------------------------------------------------------------------------------------------
@@ -833,14 +836,14 @@ int main(void) {
 
 // -------------------------------------------------------------------------------------------------------
 
-// Anzeige aktualisieren
+// Refresh LCD
 		if (SENDERBOX && flags.b.lcd_update) {
 			temp_sreg = SREG;
 			cli();
 
 			flags.b.lcd_update = 0;
 
-			// SENDER (1. Zeile + 3./4. Zeile)
+			// TRANSMITTER (1. Line + 3./4. Line)
 			lcd_cursorset(1, 1);
 			lcd_puts("Tx: ");
 			if (flags.b.tx_post) {
