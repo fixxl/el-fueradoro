8d38a9f1d95bb90bdddce013066e1486dd896a8b
f.pflaum@gmail.com <f.pflaum@gmail.com@0514aee9-97dd-25bc-fcb9-e4662e86089c>
2014-10-23 02:21:02 +0000

    New ADC-calculation routine. Now works correctly for old and new ignition devices without the need of changing the voltage divider hardware.

--- adc.c
+++ adc.c
@@ -9,48 +9,85 @@
 
 // Initialisierung
 void adc_init(void) {
-	uint16_t __attribute__ ((unused)) nosense;
+	uint16_t nosense;
 
-	// Komparator aus
+	// Comparator off
 	ACSR |= 1 << ACD;
 
-	// 1.1V als Referenz, Kanal 5 auswählen
-	ADMUX |= (1 << REFS1 | 1 << REFS0 | 1 << MUX2 | 1 << MUX0);
+	// VCC as reference, select channel 5
+	ADMUX |= (1 << REFS0 | 1 << MUX2 | 1 << MUX0);
 
-	// ADC-Vorteiler = 64 (9.8304MHz / 64 = 153kHz)
+	// ADC-Prescaler = 64 (9.8304MHz / 64 = 153kHz)
 	ADCSRA |= (1 << ADPS2 | 1 << ADPS1);
 
-	// ADC einschalten und Dummy-Wandlung durchführen
+	// Turn on ADC and execute dummy-conversion
 	ADCSRA |= 1 << ADEN;
 	ADCSRA |= 1 << ADSC;
 
-	// Auf Ende der Dummy-Wandlung warten
+	// Wait for end of dummy conversion
 	while (ADCSRA & (1 << ADSC))
 		;
 	nosense = ADCW;
+
+	_delay_ms(250);
+
+	// Check if correct reference is selected
+	nosense = 0;
+
+	for (uint8_t i = 0; i < 8; i++) {
+		ADCSRA |= 1 << ADSC;
+		while (ADCSRA & (1 << ADSC))
+			;
+		nosense += ADCW;
+	}
+	nosense >>= 3;
+
+	// Switch reference if wrong reference selection is detected
+	if (nosense < 400) {
+		ADMUX |= (1 << REFS1);
+
+		_delay_ms(250);
+
+		ADCSRA |= 1 << ADSC;
+		while (ADCSRA & (1 << ADSC))
+			;
+		nosense = ADCW;
+	}
 }
 
-// Ausführen von 11 Messungen, Mittelwertbildung,
-// Rückrechnung auf Spannungswert des Versorgers
+// Calculation of battery voltage
 uint8_t adc_read(uint8_t channel) {
 	uint32_t result = 0;
+	uint8_t imax = 10;
 
 	ADMUX &= ~(0x07);
 	ADMUX |= channel;
 
-	// 11 Messungen, Summenbildung
-	for (uint8_t i = 0; i < 11; i++) {
+	if (ADMUX & (1 << REFS1)) imax = 11;
+
+	// 10 or 11 measurements (reference-dependent), summing up
+	for (uint8_t i = 0; i < imax; i++) {
 		ADCSRA |= 1 << ADSC;
 		while (ADCSRA & (1 << ADSC))
 			;
 		result += ADCW;
 	}
-	// Mittelwertbildung durch Division durch 8 mit Rundung
-	// Umrechnung des Mittelwerts in Spannungswert (in Dezivolt) durch:
-	// U =    160/10     *   10     *    MW      * 1.1 / 1024
-	//    Spannungsteiler   Volt ->       ADC-      Umrechnung: Wert 1024
-	//      100k + 1500k     Dezivolt   Mittelwert   entspricht 1.1 Volt
+
+	// Transform ADC-value to voltage value (in dezivolt) by:
+	// 1.) U =    16          *   10     *    MW      * 1.1 / 1024
+	//    Voltage divider   Volt ->       ADC-      Transform: 1024
+	//      100k + 1500k    Dezivolt      Mean      equals 1.1 Volt
 	// Kürzen U = 16*11*MW/1024 = 11*MW/64 und runden
+	//
+	// 2.) U =    3.2         *   10     *    MW      * 5 / 1024
+	//    Voltage divider   Volt ->       ADC-     Transform: 1024
+	//      100k + 220k     Dezivolt      Mean     equals 5 Volt
+	// Kürzen U = 32*5*MW/1024 = 10*MW/64 und runden
+	//
+	// Difference between case 1 and 2 (pre-factor 11 or 10) is already taken
+	// into account during measurements
+	//
 	result = (result + 32) >> 6;
+
 	return (uint8_t) result;
 }
